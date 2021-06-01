1a2d8c4a37a08ae8caf01740780457940edbff18
xIlRosso <s290175@studenti.polito.it>
2021-06-02 01:26:21 +0200

    V2

--- solver/LargeScaleInfluence.py
+++ solver/LargeScaleInfluence.py
@@ -5,51 +5,75 @@ import gurobipy as gp
 from gurobipy import GRB
 
 
-class SimpleKnapsack():
+class LargeScaleInfluence():
     def __init__(self):
         pass
 
     def solve(
-        self, dict_data, reward, n_scenarios, time_limit=None,
+        self, dict_data, reachability, n_scenarios, time_limit=None,
         gap=None, verbose=False
     ):
-        items = range(dict_data['n_items'])
+        nodes = range(dict_data['Order'])
         scenarios = range(n_scenarios)
 
-        problem_name = "SimpleKP"
+        problem_name = "LargeSI"
         logging.info("{}".format(problem_name))
         # logging.info(f"{problem_name}")
 
         model = gp.Model(problem_name)
         X = model.addVars(
-            dict_data['n_items'],
+            dict_data['Order'], n_scenarios,
             lb=0,
             ub=1,
             vtype=GRB.INTEGER,
             name='X'
         )
-        Y = model.addVars(
-            dict_data['n_items'], n_scenarios,
+
+        Z = model.addVars(
+            dict_data['Order'],
             lb=0,
             ub=1,
             vtype=GRB.INTEGER,
-            name='Y'
+            name='Z'
         )
+
+
+
+        p=1/n_scenarios
         
-        obj_funct = gp.quicksum(dict_data["profits"][i] * X[i] for i in items)
+        #obj_funct = gp.quicksum(p * gp.quicksum(X[i][j] for j in n_scenarios) for i in nodes)
+
+        obj_funct = 0
+        for w in scenarios:
+            obj_funct += gp.quicksum(X[i,w] for i in nodes)
+            obj_funct *= p
+
+
         # for s in scenarios:
         #     obj_funct += gp.quicksum(reward[i, s] * Y[i, s] for i in items)/(n_scenarios + 0.0)
-        obj_funct += gp.quicksum(reward[i, s] * Y[i, s] for i in items for s in scenarios)/(n_scenarios + 0.0)
         model.setObjective(obj_funct, GRB.MAXIMIZE)
 
         model.addConstr(
-            gp.quicksum(dict_data['sizes'][i] * X[i] for i in items) <= dict_data['max_size'],
-            f"volume_limit_fs"
+            gp.quicksum( Z[i] for i in nodes) <= dict_data['K'],
+            f"Constraint on starting seed"
         )
+
+        model.addConstr(
+            X[i,w] <= gp.quicksum(Z[j] for j in reachability[i][:][w]) for i in nodes for j in scenarios
+        )
+
+        '''
+        model.addConstr(
+            for w in scenarios:
+                for i in nodes:
+                    ext_scenario=reachability[:][:][w]
+                    X[i,w] <= gp.quicksum(Z[j] for j in ext_scenario[i][:])
+        )
+
         
         for s in scenarios:
             model.addConstr(
-                gp.quicksum(dict_data['sizes_ss'][i] * Y[i, s] for i in items) <= dict_data['max_size_ss'],
+                gp.quicksum(dict_data['sizes_ss'][i] * Y[i, s] for i in ) <= dict_data['max_size_ss'],
                 f"volume_limit_ss_{s}"
             )
         for i in items:
@@ -57,6 +81,7 @@ class SimpleKnapsack():
                 gp.quicksum(Y[i, s] for s in scenarios) <= n_scenarios * X[i],
                 f"link_X_Y_for_item_{i}"
             )
+            '''
         model.update()
         if gap:
             model.setParam('MIPgap', gap)
@@ -74,10 +99,10 @@ class SimpleKnapsack():
         end = time.time()
         comp_time = end - start
         
-        sol = [0] * dict_data['n_items']
+        sol = [0] * dict_data['Order']
         of = -1
         if model.status == GRB.Status.OPTIMAL:
-            for i in items:
+            for i in nodes:
                 grb_var = model.getVarByName(
                     f"X[{i}]"
                 )
