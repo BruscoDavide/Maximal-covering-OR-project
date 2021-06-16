303c624c79acc8255ff59781f9b4224415fa8525
VlaStelen <vl.aelenei97@gmail.com>
2021-06-17 01:37:48 +0200

    faster heu

--- heuristic/FirstHeuristic.py
+++ heuristic/FirstHeuristic.py
@@ -4,12 +4,18 @@ import math
 import logging
 import numpy as np
 
+
+################
+import collections
+################
+
 def compute_succ(g, R, z_j):
     succ_computed=[]
     for i in range(len(R)):
-        for j in R[i]:
-            if j==z_j:
-                succ_computed.append(i)
+        #for j in R[i]:
+        #    if j==z_j:
+        if z_j in R[i]:
+            succ_computed.append(i)
     return succ_computed
 
 
@@ -23,15 +29,15 @@ class FirstHeuristic():
     ):
         sol_z = [0] * dict_data['Order']
         of = -1
-        
-        start = time.time()
-        
+
         scenarios = range(n_scenarios)
         nodes = range(dict_data['Order'])
 
         zi=[]
-
-        for w in scenarios:
+        
+        start = time.time()
+        
+        """ for w in scenarios:
             reach_scen = reachability[w]
 
             freqs = np.zeros([dict_data['Order']])
@@ -43,7 +49,31 @@ class FirstHeuristic():
             idx=list(np.argsort(freqs))
             idx.reverse()
             zi.append(idx[0:dict_data['K']])
+
+        end = time.time()
+
+        ufficial = end-start """
             
+        ##########################################
+        #start_prova = time.time()
+        for w in scenarios:
+            reach_scen = reachability[w]
+            tmp = []
+            tmp2 = []
+            for i in reach_scen:
+                tmp += i
+            
+            counter=collections.Counter(tmp)
+            common = counter.most_common(dict_data['K'])
+            for k in common:
+                tmp2.append(k[0])
+                zi.append(tmp2)
+
+        #end_prova = time.time()
+        #time_prova = end_prova - start_prova
+                
+        ##########################################
+
         of=0
         p=1/n_scenarios
         for w in scenarios:
@@ -68,7 +98,7 @@ class FirstHeuristic():
                                 
             of+=len(activated_set)*p
 
-        freqs = np.zeros([dict_data['Order']])
+        """ freqs = np.zeros([dict_data['Order']])
         for i in zi:
             for j in i:
                 freqs[j]+=1
@@ -77,7 +107,20 @@ class FirstHeuristic():
         idx.reverse()
         
         for i in idx[0:dict_data['K']]:
-            sol_z[i]=1
+            sol_z[i]=1 """
+
+        ###############################
+        asd = []
+        sol_z = []
+        for i in zi:
+            asd+=i
+        freqs = collections.Counter(asd)
+        zs = freqs.most_common(dict_data['K'])
+        for k in zs:
+            sol_z.append(k[0])
+
+        ###############################
+
 
         end = time.time()
 
